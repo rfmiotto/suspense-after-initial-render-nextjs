911361c8c263a0cbf6b15d5b3a11f0898483bfef
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2022-03-13 21:12:05 -0300

    boilerplate way to solve our spinner problem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,44 +20,63 @@ function ErrorFallback() {
 }
 
 /*
-In this example, when we load the page for the first time (or during a refresh),
-we see a loading spinner. This app was already wired up using Suspense, and we
-have 2 components: a Sidebar and a component on the right that display the message.
-As the Sidebar component fetches its own messages, we see that root suspense
-boundary rendering the spinner while both components are loading their data.
-But, there is something funny going on: once you start click different messages
-that haven't been loaded yet, we fallback to that root suspense boundary, because
-that is how Suspense works. Anytime a component fetches data with a Suspense,
-React will render the next Suspense boundaries. Here, we don't want to render
-the Suspense root boundary because it gives us a weird behavior.
-Fortunately, Suspense supports nested boundaries, so we can solve this problem
-by adding another boundary wrapping our `Component`. Now, the Component, which
-in our case is going to be the Message component, is the only component fetching
-data and React will leave the Sidebar render.
-But now look at what happens when we refresh the page when a message is selected:
-we have this sort of waterfall spinner problem whenever we start at an specific
-message. After refreshing the page, we see the main spinner, then the Sidebar is
-rendered and since the message takes longer to load, we see another spinner.
-Here the Sidebar request is finishing first, so React will go ahead and render it,
-but since the Message takes longer to complete its request and it is wrapped under
-another Suspense boundary, we see that loading spinner.
-So we are kind of in a middle of a pickle here: we want the inner Suspense boundary
-to exist so that we keep the sidebar always rendered while the messages are being
-loaded, but if we have a message selected in initial render, we only want one
-spinner. How can we solve this problem?
+Okay, we want the inner Suspense boundary to exist so that we keep the sidebar 
+always rendered while the messages are being loaded, but if we have a message
+selected in initial render, we only want one spinner.
+Really what we want is 2 different trees based on whether it is the initial render.
+So if we had some state here to store if it is an initial render, we would be
+all set. But do we know when this initial render has finished?
+*We can use the fact that side effects don't run within a Suspense boundary until*
+*all of the components have finished suspending.*
+To exemplify this, let's create a NewComponent and place it inside a Suspense
+Boundary. Notice that the message "MyApp rendered" will be logged in the console
+but the "NewComponent rendered" message will only be logged after all these
+components finish suspending. That only occurs because NewComponent is sitting
+inside a Suspense tree. We can use this fact to pass down a callback that says
+"hey, it's not an initial render anymore".
+This solution basically solves our problem, but there is a lot of boilerplate
+here: we have this isInitialRender state, we have this NewComponent, and also
+we are duplicating our Component (the Message) because of this two different
+trees.
+Let's refactor this code...
 */
 
+function NewComponent({ afterRender }: { afterRender: () => void }) {
+  useEffect(() => {
+    console.log("NewComponent rendered");
+    afterRender();
+  }, []);
+
+  return null;
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isInitialRender, setIsInitialRender] = useState(true);
+
+  useEffect(() => {
+    console.log("MyApp rendered");
+  }, []);
+
   return (
     <div className="flex h-screen bg-zinc-800 text-zinc-100 antialiased">
       <Suspense fallback={<Spinner />}>
         <QueryClientProvider client={queryClient}>
           <Sidebar />
 
+          <NewComponent
+            afterRender={() => {
+              setIsInitialRender(false);
+            }}
+          />
+
           <div className="flex w-full bg-zinc-900">
-            <Suspense fallback={<Spinner />}>
+            {isInitialRender ? (
               <Component {...pageProps} />
-            </Suspense>
+            ) : (
+              <Suspense fallback={<Spinner />}>
+                <Component {...pageProps} />
+              </Suspense>
+            )}
           </div>
 
           <ReactQueryDevtools />
